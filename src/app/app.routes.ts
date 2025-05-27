5207c0e59840e6ca2c316d74cd2505e44699ebba
markuczy <129275100+markuczy@users.noreply.github.com>
2025-05-27 19:25:17 +0200

    fix: remove content

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { GreetingsListComponent } from './greetings-list/greetings-list.component';
-import { GreetingDetailComponent } from './greeting-detail/greeting-detail.component';
-
-export const routes: Routes = [
-  { path: '', component: GreetingsListComponent },
-  { path: 'greeting/:id', component: GreetingDetailComponent },
-];
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(null, {
-  providers: [provideRouter(routes)],
-});
